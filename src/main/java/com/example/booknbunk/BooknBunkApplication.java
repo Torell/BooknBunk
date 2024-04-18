5ce7bd5000470e38a2eca8a8b483a08ba4e3cafd
Rasmus Torell <rasmus.torell@gmail.com>
2024-04-18 15:22:57 +0200

    Initial commit

---
+++ src/main/java/com/example/booknbunk/BooknBunkApplication.java
@@ -0,0 +1,13 @@
+package com.example.booknbunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknBunkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooknBunkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/booknbunk/BooknBunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booknbunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknBunkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
