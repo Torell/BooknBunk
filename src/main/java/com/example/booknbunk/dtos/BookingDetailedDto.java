d0a785cfca5170f4899762c8bea778566cde1c0a
Rasmus Torell <rasmus.torell@gmail.com>
2024-04-22 10:54:59 +0200

    Added simple dtos

---
+++ src/main/java/com/example/booknbunk/dtos/BookingDetailedDto.java
@@ -0,0 +1,14 @@
+package com.example.booknbunk.dtos;
+
+import com.example.booknbunk.models.Room;
+
+import java.time.Period;
+
+public class BookingDetailedDto {
+    private Period bookingPeriod;
+    private CustomerDto customerDto;
+
+    private int extraBed;
+
+
+}
---
+++ src/main/java/com/example/booknbunk/dtos/BookingDto.java
@@ -0,0 +1,9 @@
+package com.example.booknbunk.dtos;
+
+import java.time.Period;
+
+public class BookingDto {
+
+    private Period bookingPeriod;
+    private int roomSize;
+}
