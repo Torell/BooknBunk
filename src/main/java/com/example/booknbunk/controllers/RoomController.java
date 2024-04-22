99db563231a44064808422d6b673433d7fd610b3
Stephanie <stephanie.zevian@yh.nackademin.se>
2024-04-22 10:07:34 +0200

    La till controller, repo för rum, och boostrappat in data för rummen.

---
+++ src/main/java/com/example/booknbunk/controllers/RoomController.java
@@ -0,0 +1,13 @@
+package com.example.booknbunk.controllers;
+
+import com.example.booknbunk.repositories.RoomRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class RoomController {
+
+   private final RoomRepository roomRepository;
+
+}
