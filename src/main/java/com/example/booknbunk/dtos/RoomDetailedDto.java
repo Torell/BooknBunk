3598309f9b3bbf9c0049b64b746f0af2d2b57b99
Stephanie <stephanie.zevian@yh.nackademin.se>
2024-04-22 11:02:32 +0200

    Lagt till början av RoomDto och RoomDetailedDto

---
+++ src/main/java/com/example/booknbunk/dtos/RoomDetailedDto.java
@@ -0,0 +1,17 @@
+package com.example.booknbunk.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoomDetailedDto {
+
+    private Long id;
+
+    private int RoomSize;
+}
---
+++ src/main/java/com/example/booknbunk/dtos/RoomDto.java
@@ -0,0 +1,6 @@
+package com.example.booknbunk.dtos;
+
+public class RoomDto {
+
+    private int roomSize;
+}
