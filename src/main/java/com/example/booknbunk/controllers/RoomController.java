2219a6f38472ab4777a80467db091c8e275a4d6a
Anna Boronkai <annaboronki@hotmail.com>
2024-04-26 15:40:11 +0200

    .

--- src/main/java/com/example/booknbunk/controllers/RoomController.java
+++ src/main/java/com/example/booknbunk/controllers/RoomController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Controller
-@RestController
 @AllArgsConstructor
+@RequestMapping("/room")
 public class RoomController {
 
    private final RoomRepository roomRepository;
