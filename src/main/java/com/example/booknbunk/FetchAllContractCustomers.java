1668d966c598b2fe1240f2cfd8d061bac5cc0d47
Rasmus Torell <rasmus.torell@gmail.com>
2024-05-08 13:27:43 +0200

    Added ContractCustomer Added ContractCustomerDetailedDTO Added service layer added conversion methods

---
+++ src/main/java/com/example/booknbunk/FetchAllContractCustomers.java
@@ -0,0 +1,13 @@
+package com.example.booknbunk;
+
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import org.springframework.boot.CommandLineRunner;
+
+public class FetchAllContractCustomers implements CommandLineRunner {
+
+    @Override
+    public void run(String...args) {
+        JacksonXmlModule jacksonXmlModule = new JacksonXmlModule();
+
+    }
+}
---
+++ src/main/java/com/example/booknbunk/services/implementations/ContractCustomerServiceImplementation.java
@@ -0,0 +1,42 @@
+package com.example.booknbunk.services.implementations;
+
+import com.example.booknbunk.dtos.ContractCustomerDetailedDTO;
+import com.example.booknbunk.models.ContractCustomer;
+import com.example.booknbunk.services.interfaces.ContractCustomerService;
+
+public class ContractCustomerServiceImplementation implements ContractCustomerService {
+
+    @Override
+    public ContractCustomerDetailedDTO contractCustomerToDetailedDTO(ContractCustomer contractCustomer) {
+        return ContractCustomerDetailedDTO.builder()
+                .id(contractCustomer.getId())
+                .externalSystemId(contractCustomer.getExternalSystemId())
+                .companyName(contractCustomer.getCompanyName())
+                .contactTitle(contractCustomer.getContactTitle())
+                .streetAddress(contractCustomer.getStreetAddress())
+                .city(contractCustomer.getCity())
+                .postalCode(contractCustomer.getPostalCode())
+                .country(contractCustomer.getCountry())
+                .contactName(contractCustomer.getContactName())
+                .phone(contractCustomer.getPhone())
+                .fax(contractCustomer.getFax())
+                .build();
+    }
+
+    @Override
+    public ContractCustomer detailedDtotoContractCustomer(ContractCustomerDetailedDTO contractCustomerDetailedDTO) {
+        return ContractCustomer.builder()
+                .id(contractCustomerDetailedDTO.getId())
+                .externalSystemId(contractCustomerDetailedDTO.getExternalSystemId())
+                .companyName(contractCustomerDetailedDTO.getCompanyName())
+                .contactTitle(contractCustomerDetailedDTO.getContactTitle())
+                .streetAddress(contractCustomerDetailedDTO.getStreetAddress())
+                .city(contractCustomerDetailedDTO.getCity())
+                .postalCode(contractCustomerDetailedDTO.getPostalCode())
+                .country(contractCustomerDetailedDTO.getCountry())
+                .contactName(contractCustomerDetailedDTO.getContactName())
+                .phone(contractCustomerDetailedDTO.getPhone())
+                .fax(contractCustomerDetailedDTO.getFax())
+                .build();
+    }
+}
