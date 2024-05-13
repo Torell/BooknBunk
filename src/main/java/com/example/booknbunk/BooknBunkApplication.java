63bcc6bd2fdb6094e3f5b2f158a97d92ae12cf36
Rasmus Torell <rasmus.torell@gmail.com>
2024-05-13 10:02:43 +0200

    Sort and search works

--- src/main/java/com/example/booknbunk/BooknBunkApplication.java
+++ src/main/java/com/example/booknbunk/BooknBunkApplication.java
@@ -24,7 +24,7 @@ public class BooknBunkApplication {
     public static void main(String[] args) {
         if (args.length == 0)
             SpringApplication.run(BooknBunkApplication.class, args);
-        else if (Objects.equals(args[0], "fetchallcustomers")) {
+        else if (Objects.equals(args[0], "fetch")) {
             SpringApplication springApplication = new SpringApplication(FetchAllContractCustomers.class);
             springApplication.setWebApplicationType(WebApplicationType.NONE);
             springApplication.run(args);
