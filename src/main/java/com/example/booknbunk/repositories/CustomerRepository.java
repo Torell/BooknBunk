919e56668b3c1360fa2107c6b7563e7c87196e78
Rasmus Torell <rasmus.torell@gmail.com>
2024-05-20 10:52:09 +0200

    Created integration test for contractCustomer

---
+++ src/main/java/com/example/booknbunk/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.booknbunk.repositories;
+
+import com.example.booknbunk.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
+}
