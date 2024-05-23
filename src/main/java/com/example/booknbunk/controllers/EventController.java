de299febd9555e3b7d08d40df937cc2dc89d63f0
Anna Boronkai <annaboronki@hotmail.com>
2024-05-23 13:20:52 +0200

    lade till controller för events

---
+++ src/main/java/com/example/booknbunk/controllers/EventController.java
@@ -0,0 +1,25 @@
+import com.example.booknbunk.dtos.EventDto;
+import com.example.booknbunk.services.interfaces.EventService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping("/rooms/{id}/events")
+    public String showEventsForRoom(@PathVariable Long id, Model model){
+        List<EventDto> events = eventService.getAllEventsDtoByRoomId(id);
+        model.addAttribute("events", events);
+        return "/room/roomEvents";
+    }
+
+}
