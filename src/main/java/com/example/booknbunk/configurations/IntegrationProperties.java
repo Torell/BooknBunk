76d8f3d1ed7e8c664c99ac4b650d350dc41377d4
Anna Boronkai <annaboronki@hotmail.com>
2024-05-28 11:48:20 +0200

    injects IntegrationProperties

--- src/main/java/com/example/booknbunk/configurations/IntegrationProperties.java
+++ src/main/java/com/example/booknbunk/configurations/IntegrationProperties.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @ConfigurationPropertiesScan
 @Configuration
