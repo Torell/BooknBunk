eb69bd5a7fe294e012557029044a0a4b5e0b9099
Rasmus Torell <rasmus.torell@gmail.com>
2024-05-29 18:45:22 +0200

    added annotations fetchContractCustomers no longer always run (removed @component)

--- src/main/java/com/example/booknbunk/FetchAllEvents.java
+++ src/main/java/com/example/booknbunk/FetchAllEvents.java
@@ -1,23 +1,17 @@
 package com.example.booknbunk;
 
 import com.example.booknbunk.configurations.IntegrationProperties;
-import com.example.booknbunk.models.Event;
-import com.example.booknbunk.repositories.EventRepository;
-import com.example.booknbunk.repositories.RoomRepository;
 import com.example.booknbunk.services.interfaces.EventService;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+
 
 @Component
 @ComponentScan
@@ -25,14 +19,12 @@ public class FetchAllEvents implements CommandLineRunner {
 
     private final EventService eventService;
 
-    @Autowired
     IntegrationProperties integrationProperties;
 
-    public FetchAllEvents(EventService eventService) {
+    public FetchAllEvents(EventService eventService, IntegrationProperties integrationProperties) {
         this.eventService = eventService;
+        this.integrationProperties = integrationProperties;
     }
-
-
 
     @Override
     public void run(String... args) throws Exception {
@@ -45,7 +37,7 @@ public class FetchAllEvents implements CommandLineRunner {
 
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
             System.out.println(" [x] Received '" + message + "'");
             eventService.processEvent(message);
         };
